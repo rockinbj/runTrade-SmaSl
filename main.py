b84b4b81b67bce29cf7d993ad15b11c1d157d81b
Rock <luxxsys@gmail.com>
2023-02-08 12:03:57 +0800

    adjust main process order

--- main.py
+++ main.py
@@ -42,11 +42,6 @@ def main():
         # 获取币池列表
         mkts = ex.loadMarkets()
         logger.info(f"获取币种信息完毕, 共 {len(mkts)}")
-        # 获取当前余额
-        balance = getBalance(exchange=ex, asset=RULE)
-        logger.info(f"获取余额信息完毕, 当前资金 {round(balance, 2)}, "
-                    f"可开资金 {round(balance * MAX_BALANCE, 2)}, "
-                    f"页面杠杆 {LEVERAGE}, 实际杠杆 {round(MAX_BALANCE * LEVERAGE, 2)}")
 
         # 获取当前持仓
         posNow = getOpenPosition(exchange=ex)
@@ -59,6 +54,14 @@ def main():
             offsetSec=OFFSET_SEC,
             test=IS_TEST,
         )
+
+        # 获取当前余额
+        balance = getBalance(exchange=ex, asset=RULE)
+        logger.info(
+            f"获取余额信息完毕, 当前资金 {round(balance, 2)}, "
+            f"可开资金 {round(balance * MAX_BALANCE, 2)}, "
+            f"页面杠杆 {LEVERAGE}, 实际杠杆 {round(MAX_BALANCE * LEVERAGE, 2)}"
+            )
         if MARKET == "spot" and balance <= MIN_SPOT_BALANCE:
             logger.info(f"可用资金低于余额下限 {MIN_SPOT_BALANCE}U, 本轮不交易")
             continue
